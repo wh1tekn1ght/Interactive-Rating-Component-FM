79b9f76aa7b80ab52d21f0fc8cbde13814fbc96d
fermendoza18 <fernando.ffme2015@gmail.com>
2022-11-17 20:22:07 -0600

    JS code improved

--- main.js
+++ main.js
@@ -1,71 +1,23 @@
-const inputOne = document.querySelector("#one");
-const inputTwo = document.querySelector("#two");
-const inputThree = document.querySelector("#three");
-const inputFour = document.querySelector("#four");
-const inputFive = document.querySelector("#five");
+const ratingNumbers = document.querySelectorAll(".rating-values");
 const button = document.querySelector(".primary-button");
 const ratingContainer = document.querySelector(".rating-container");
 const feedbackContainer = document.querySelector(".feedback-container");
 const score = document.querySelector('#score');
 let value;
 
-inputOne.addEventListener('click', checkedToggle);
-inputTwo.addEventListener('click', checkedToggle2);
-inputThree.addEventListener('click', checkedToggle3);
-inputFour.addEventListener('click', checkedToggle4);
-inputFive.addEventListener('click', checkedToggle5);
-
-function checkedToggle() {
-  inputOne.classList.toggle('clicked');
-  inputTwo.classList.remove('clicked');
-  inputThree.classList.remove('clicked');
-  inputFour.classList.remove('clicked');
-  inputFive.classList.remove('clicked');
-  value = 1;
-}
-
-function checkedToggle2() {
-  inputTwo.classList.toggle('clicked');
-  inputOne.classList.remove('clicked');
-  inputThree.classList.remove('clicked');
-  inputFour.classList.remove('clicked');
-  inputFive.classList.remove('clicked');
-  value = 2;
-}
+ratingNumbers.forEach((element) => {
+  element.addEventListener("click", () => {
+     ratingNumbers.forEach((element) => {
+       element.classList.remove("clicked");
+     });
+     element.classList.add("clicked");
+     value = element.innerText;
+  });
+});
 
 
-function checkedToggle3() {
-  inputThree.classList.toggle('clicked');
-  inputOne.classList.remove('clicked');
-  inputTwo.classList.remove('clicked');
-  inputFour.classList.remove('clicked');
-  inputFive.classList.remove('clicked');
-  value = 3;
-}
-
-
-function checkedToggle4() {
-  inputFour.classList.toggle('clicked');
-  inputOne.classList.remove('clicked');
-  inputTwo.classList.remove('clicked');
-  inputThree.classList.remove('clicked');
-  inputFive.classList.remove('clicked');
-  value = 4;
-}
-
-function checkedToggle5() {
-  inputFive.classList.toggle('clicked');
-  inputOne.classList.remove('clicked');
-  inputTwo.classList.remove('clicked');
-  inputThree.classList.remove('clicked');
-  inputFour.classList.remove('clicked');
-  value = 5;
-}
-
-button.addEventListener('click', showFeedback);
-
-function showFeedback() {
+button.addEventListener("click", function () {
   score.innerText = value;
-  ratingContainer.classList.add('inactive');
-  feedbackContainer.classList.remove('inactive');
-}
+  ratingContainer.classList.add("inactive");
+  feedbackContainer.classList.remove("inactive");
+});
